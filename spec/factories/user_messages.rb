28bce00f44cc838cb3a6ebe95dfe367501ddc494
Erlinis Quintana <ing.erlinisquintana@gmail.com>
2013-11-07 18:33:20 -0500

    New factories to test image

--- spec/factories/user_messages.rb
+++ spec/factories/user_messages.rb
@@ -10,6 +10,16 @@ FactoryGirl.define do
   factory :invalid_user_message, parent: :user_message do
     message nil
   end
-  
+
+  factory :user_message_with_image, parent: :user_message do
+    message "message with image"
+    image File.open(File.join(Rails.root, '/spec/fixtures/files/image_allowed_ext.jpeg'))
+  end
+
+  factory :user_message_with_image_not_allowed_ext, parent: :user_message do
+    message :random_message
+    image File.open(File.join(Rails.root, '/spec/fixtures/files/image_not_allowed_ext.bmp'))
+  end
+
 end
 
