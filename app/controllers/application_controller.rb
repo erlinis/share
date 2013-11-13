e499c5dcfc4f1606f9bd7714aaa8c875f4551591
Erlinis Quintana <ing.erlinisquintana@gmail.com>
2013-11-13 11:36:50 -0500

    rescue method moved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
   def render_404
     respond_to do |format|
       format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
