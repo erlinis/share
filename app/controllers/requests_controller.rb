aaeea555d9220a72893d53f85b4bbd0b0a179118
Ricardo <rickardoberdejo@hotmail.com>
2013-11-14 16:33:18 -0500

    added validation to check the existence of receiver before save since rails does not do that for me. Added users controller with index and show action to list registered o single user + tests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,4 +5,14 @@ class RequestsController < ApplicationController
 	def index
 		@requests = current_user.requests
 	end
+
+	def create
+		@request = current_user.requests.build(:receiver_id => params[:friend_id])
+		if @request.save
+			flash[:notice] = 'Successfully Saved'
+		else
+			flash[:error] = "Error"
+		end
+		redirect_to users_path
+	end
 end
